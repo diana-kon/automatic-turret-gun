47c284c4eab5c82522fe9aa6d0f9d82c7597be63
Diana <dija6677@gmail.com>
2025-06-02 16:03:42 +0100

    messing around 0.1

--- main.py
+++ main.py
@@ -13,27 +13,42 @@ def angle(current, step):
         step = 0 - step
     return (current + step, step)
 
-def move(a, step, n):
-    (a, step) = angle(a, step)
+#def move(a, step, n):
+#    (a, step) = angle(a, step)
     # do not control the pan-tilt for now
     # pantilthat.pan(a)
     # pantilthat.tilt(-20)
-    if (a % 45 == 0 or a == 0):
-        blinkt.set_pixel(0, 156, 2, 163, 0.2)
-        blinkt.show()
-        time.sleep(2)
-        blinkt.set_pixel(1, 0, 0, 0)
-        blinkt.show()
-    else:
-        time.sleep(0.003)
-    return (a, step, n + 1)
+    #if (a % 45 == 0 or a == 0):
+    #    blinkt.set_pixel(0, 156, 2, 163, 0.2)
+    #    blinkt.show()
+    #    time.sleep(2)
+    #    blinkt.set_pixel(1, 0, 0, 0)
+    #    blinkt.show()
+    #else:
+    #    time.sleep(0.003)
+    #return (a, step, n + 1)
+
+
 
 def blink(timestamp):
-    if (timestamp % 2 == 0):
-        blinkt.set_pixel(1, 199, 38, 3, 0.1)
-    else:
-        blinkt.set_pixel(0, 0, 0, 0)
-    blinkt.show()
+    while True:
+        for i in range(8):
+            clear()
+            set_pixel(i, 255, 0, 0)
+            show()
+            time.sleep(0.05)
+            #blinkt.show()
+
+
+
+
+
+#def blink(timestamp):
+#    if (timestamp % 2 == 0):
+#        blinkt.set_pixel(1, 199, 38, 3, 0.1)
+#    else:
+#        blinkt.set_pixel(0, 0, 0, 0)
+#    blinkt.show()
 
 #Log_S
 
